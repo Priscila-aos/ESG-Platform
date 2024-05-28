15b3fb47c6ad5e9842ce1726060cbeb8f8d95a44
Priscila Oliveira <91674137+Priscila-aos@users.noreply.github.com>
2024-05-28 14:45:42 -0300

    Feat:criação de pastas contexts e hooks para autenticação

--- esg/src/App.js
+++ esg/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import GlobalStyle from './styles/global'
 import RoutesApp from './routers'
+import { AuthProvider } from './contexts/auth'
 
 //rafce
 const App = () => {
   return (
-  <>
+  <AuthProvider>
     <RoutesApp />
     <GlobalStyle />
-  </>
+  </AuthProvider>
   )
 }
 
-export default App;
+export default App
