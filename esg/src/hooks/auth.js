117e75ca3003b3d2d2c52e24b513ee07951a3d8b
Priscila Oliveira <91674137+Priscila-aos@users.noreply.github.com>
2024-05-28 16:41:55 -0300

    Refactor: criação das pastas contexts e hooks para autenticação

--- esg/src/hooks/auth.js
+++ esg/src/hooks/auth.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useContext } from 'react'
 import {AuthContext} from '../contexts/auth'
 
-
+//refct e rafec
 const useAuth = () => {
     const context = useContext(AuthContext)   
     return context
